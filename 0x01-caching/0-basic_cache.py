2a8b0c588b81560ee9902c8d3a11ebba8af12640
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-21 02:16:26 -0700

    Task0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Basic dictionary
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache """
+    def put(self, key, item):
+        """ Assigns the new item to the dictionary """
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returns the value in self.cache_data linked to key """
+        if key is None or not (key in self.cache_data):
+            return None
+        return self.cache_data[key]
